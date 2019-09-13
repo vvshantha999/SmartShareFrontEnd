07fabd3c8cb8b265764916e731d19a9fd9c1bc57
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2019-09-13 07:40:58 +0100

    post request for signup functionality is completed

---
+++ src/app/authentication/authentication.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {User} from './domain-models/User';
+import {Status} from './domain-models/Status';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+  private signUpUrl = 'http://localhost:8081/signUp';
+
+  constructor(private httpService: HttpClient) {
+  }
+
+  registerUser(userInfo: User): Observable<Status> {
+    return this.httpService.post<Status>(this.signUpUrl, userInfo, this.httpOptions).pipe(
+      catchError(this.handleError<Status>('registerUser', null))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    };
+  }
+}
